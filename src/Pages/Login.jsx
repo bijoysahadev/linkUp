9a4fc88da0cf44a9bfc0265c0a45377fbd957989
bijoysahadev <bijoysahamernstackdev@gmail.com>
2026-06-11 20:39:38 +0600

    Your custom commit message here

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,23 +1,83 @@
 import React from 'react'
 import Grid from '@mui/material/Grid';
+import Image from '../Componets/Image'
+import TextField from '@mui/material/TextField';
+import login from "../assets/login.png";
+import Button from '@mui/material/Button';
+import styled from '@emotion/styled';
+import { FcGoogle } from "react-icons/fc";
+import { Link } from 'react-router-dom';
+
+const ButtonCustomize = styled(Button) ({
+ padding : '20px 0px',
+ width : "80%",
+ background: '#5B36F5',
+ fontSize : "20px",
+ fontWeight : "600",
+ fontFamily: "",
+ borderRadius : "87px",
+ textTransform : "capitalize",
+  marginTop : '51px',
+  marginBottom : '31px'
+})
+const TextFieldCustomize = styled(TextField) ({
+  marginTop :  '50px',
+   width : '80%',
+   borderRadius : '10px',
+   borderWidth : '0px',
+     '& .MuiInput-underline:after': {
+    borderBottomWidth: '0px',
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderWidth: '1px',
+      borderRadius : '14px'
+    },
+    '&:hover fieldset': {
+      borderWidth: '1px',
+          borderRadius : '14px'
+    },
+    '&.Mui-focused fieldset': {
+      borderWidth: '1px',
+          borderRadius : '14px'
+    },
+    '& .MuiInput-underline:after': {
+    borderBottomColor: '#B2BAC2',
+  },
+  }
+})
+
+
+
 const Login = () => {
   return (
-<>  
-
-<Grid container spacing={2}>
+   <>
+   <Grid container spacing={2}>
         <Grid size={6}>
-        sdfdsf
+        <div className='flex items-center justify-end h-screen '>
+           <div className='w-140' >
+          <h1 className='text-[#11175D]  font-bold text-[34px] ' >Login to your account!</h1>
+          <Button sx={{padding: "12px 30px ", marginTop: "20px "}} variant="outlined" startIcon={<FcGoogle />}>
+        Login with Google
+      </Button>
+              <TextFieldCustomize   type='email' placeholder='Enter your name' id="standard-basic" label="Email Addres" variant="standard" />
+              <TextFieldCustomize  type='password' id="standard-basic" label="Password" placeholder='Enter Your Password'   variant="standard" />
+            <br />
+            <ButtonCustomize variant="contained">Login to Continue</ButtonCustomize>
+            <p  className=' ml-24 mb-10 text-base cursor-pointer text-black font-bold' >Forget Password</p>
+          
+             <p className=' ml-16  cursor-pointer  text-start   text-sm text-[#03014C] font-normal  ' >Don’t have an account ? <Link to={"/Regestration"}><span className='text-[#EA6C00]'>Sign up  </span> </Link> </p>
+        
+        </div>
+        </div>
         </Grid>
         <Grid size={6}>
-            adsdsasdd
+           <Image  className={`w-full h-screen object-cover`}  src={login}/>
         </Grid>
      
         
       </Grid>
-
-
- 
-</>
+   </>
   )
 }
 
--- src/Pages/Regestration.jsx
+++ src/Pages/Regestration.jsx
@@ -1,24 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Grid from '@mui/material/Grid';
 import Image from '../Componets/Image'
 import TextField from '@mui/material/TextField';
 import Login from "../assets/signUp.png";
 import Button from '@mui/material/Button';
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+
+const ButtonCustomize = styled(Button) ({
+ padding : '20px 0px',
+ width : "80%",
+ background: '#5B36F5',
+ fontSize : "20px",
+ fontWeight : "600",
+ fontFamily: "",
+ borderRadius : "87px",
+ textTransform : "capitalize",
+  marginTop : '51px',
+  marginBottom : '31px'
+})
+const TextFieldCustomize = styled(TextField) ({
+  marginTop :  '30px',
+   width : '80%',
+   borderRadius : '10px',
+   borderWidth : '0px',
+     '& .MuiInput-underline:after': {
+    borderBottomWidth: '0px',
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderWidth: '1px',
+      borderRadius : '14px'
+    },
+    '&:hover fieldset': {
+      borderWidth: '1px',
+          borderRadius : '14px'
+    },
+    '&.Mui-focused fieldset': {
+      borderWidth: '1px',
+          borderRadius : '14px'
+    },
+    '& .MuiInput-underline:after': {
+    borderBottomColor: '#B2BAC2',
+  },
+  }
+})
+
+
+
 const Regestration = () => {
+  let [email,setEmail]=useState ("")
+  let [name,setName]=useState(" ")
+  let [password,setPassword]=(" ")
   return (
    <>
    <Grid container spacing={2}>
         <Grid size={6}>
         <div className='flex items-center justify-end h-screen '>
-           <div className='w-130' >
+    
+           <div className='w-140' >
           <h1 className='text-[#11175D]  font-bold text-[34px] ' >Get started with easily register</h1>
-          <p className='text-[20px] pt-3 pb-10 font-black font-normal' >Free register and you can enjoy it</p>
-            <TextField  sx={{width:"80%"}}   id="Email Address" label="Email Address" variant="outlined" />
-            <TextField  sx={{width:"80%"}}   id="outlined-basic" label="Ful name" variant="outlined" />
-            <TextField  sx={{width:"80%"}}   id="outlined-basic" label="Password" variant="outlined" />
+          <p className='  curspor-pointer text-[20px] pt-3 pb-10 font-black font-normal' >Free register and you can enjoy it</p>
+            <TextFieldCustomize  value={name} onChange={()=>setEmail(e.target.value)} type='text' id="Email Address"
+            label="Email Address" variant="outlined" />
+            <TextFieldCustomize  onChange={()=>setName(e.target.value)} type='text'  id="outlined-basic" label="Ful name" variant="outlined" />
+            <TextFieldCustomize   onChange={()=>setPassword(e.target.value)} type='password' id="outlined-basic" label="Password" variant="outlined" />
             <br />
-            <Button variant="contained">Sign up</Button>
-            <p>Already  have an account ? Sign In</p>
+            <ButtonCustomize variant="contained">Sign up</ButtonCustomize>
+           <div className='w-8/12' >
+             <p className='  text-center   text-sm text-[#03014C] font-normal  ' >Already  have an account ?   <Link to="/Login" ><span className='text-[#EA6C00]'>Sign In</span></Link>  </p>
+           </div>
         </div>
         </div>
         </Grid>
