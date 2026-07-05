448e8cc7d27c697f444aaafde4735ab0787a710a
bijoysahadev <bijoysahamernstackdev@gmail.com>
2026-07-05 16:32:18 +0600

    feat:addded states to icons and fixed isssues

--- src/Componets/Sidebar.jsx
+++ src/Componets/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Profile from '../assets/Profile.jpg';
 import { IoHomeOutline } from "react-icons/io5";
 import Image from './Image';
@@ -7,8 +7,10 @@ import { IoNotificationsOff } from "react-icons/io5";
 import { IoSettingsOutline } from "react-icons/io5";
 import { LuLogOut } from "react-icons/lu";
 const Sidebar = () => {
+   let [active,setActive]=useState(false)
   let handleList = ()=> {
     console.log("clicked");
+    setActive(true)
     
   }
   return (
@@ -18,17 +20,27 @@ const Sidebar = () => {
           <Image   src={Profile} className={`w-full h-full rounded-full`} /> </div>
         <div  className=''   >
           <ul className='flex flex-col gap-y-10 '   >
-            <li onClick={handleList} className='text-5xl   relative  after:absolute after:left-[67px] after:top-1/2  after:w-[160px] after:h-16  after:bg-[white] after:content-[""]  after:rounded-l-md text-[#5F35F5]  after:-translate-1/2   before:absolute before:top-1/2 before:-translate-y-1/2  before:-right-25 before:bg-[#5F35F5] before:w-[10px] before:h-16 before:content-[""]  before:rounded-l-xl  before:z-19 ' >  <IoHomeOutline  className='relative top-0 left-0 z-10'   /></li>
-            <li onClick={handleList} className='text-5xl   relative  after:absolute after:left-[67px] after:top-1/2  after:w-[160px] after:h-16  after:bg-[white] after:content-[""]  after:rounded-l-md text-[#5F35F5]  after:-translate-1/2   before:absolute before:top-1/2 before:-translate-y-1/2  before:-right-25 before:bg-[#5F35F5] before:w-[10px] before:h-16 before:content-[""]  before:rounded-l-xl  before:z-19 ' >  <LuMessageCircleMore  className='relative top-0 left-0 z-10' /></li>
-            <li onClick={handleList} className='text-5xl   relative  after:absolute after:left-[67px] after:top-1/2  after:w-[160px] after:h-16  after:bg-[white] after:content-[""]  after:rounded-l-md text-[#5F35F5]  after:-translate-1/2   before:absolute before:top-1/2 before:-translate-y-1/2  before:-right-25 before:bg-[#5F35F5] before:w-[10px] before:h-16 before:content-[""]  before:rounded-l-xl  before:z-19 ' > <IoNotificationsOff  className='relative top-0 left-0 z-10'   /></li>
-            <li onClick={handleList} className='text-5xl   relative  after:absolute after:left-[67px] after:top-1/2  after:w-[160px] after:h-16  after:bg-[white] after:content-[""]  after:rounded-l-md text-[#5F35F5]  after:-translate-1/2   before:absolute before:top-1/2 before:-translate-y-1/2  before:-right-25 before:bg-[#5F35F5] before:w-[10px] before:h-16 before:content-[""]  before:rounded-l-xl  before:z-19 ' >  <IoSettingsOutline   className='relative top-0 left-0 z-10'  /> </li>
+            <li onClick={handleList} className={`text-5xl   relative  after:absolute after:left-[67px] after:top-1/2  after:w-[160px] after:h-16  ${active ? "after:bg-[white]": "after:bg-transparent"} after:content-[""]  after:rounded-l-md ${active ? "text-[#5F35F5] ": "text-white"} after:-translate-1/2   before:absolute before:top-1/2 before:-translate-y-1/2  before:-right-25 ${active ? "before:bg-[#5F35F5]" : "bg-transparent"} before:w-[10px] before:h-16 before:content-[""]  before:rounded-l-xl  before:z-19`} >  <IoHomeOutline  className='relative top-0 left-0 z-10'   /></li>
+
+
+            <li onClick={handleList} className={`text-5xl   relative  after:absolute after:left-[67px] after:top-1/2  after:w-[160px] after:h-16  ${active ? " after:bg-[white]" :"after:bg-transparent"}  after:content-[""]  after:rounded-l-md ${active ? "text-[#5F35F5] ": "text-white"} after:-translate-1/2   before:absolute before:top-1/2 before:-translate-y-1/2  before:-right-25  ${active ? "before:bg-[#5F35F5] " :"before:bg-transparent"} before:w-[10px] before:h-16 before:content-[""]  before:rounded-l-xl  before:z-19`} >  <LuMessageCircleMore  className='relative top-0 left-0 z-10' /></li>
+
+
+
+
+
+
+            
+            <li onClick={handleList} className={`text-5xl   relative  after:absolute after:left-[67px] after:top-1/2  after:w-[160px] after:h-16  ${active ? "after:bg-[white]": "after:bg-transparent"} after:content-[""]  after:rounded-l-md  ${active? "text-[#5F35F5]  " :"text-white"} after:-translate-1/2   before:absolute before:top-1/2 before:-translate-y-1/2  before:-right-25  ${active ? "before:bg-[#5F35F5]": "before:bg-transparent"} before:w-[10px] before:h-16 before:content-[""]  before:rounded-l-xl  before:z-19`} > <IoNotificationsOff  className='relative top-0 left-0 z-10'   /></li>
+            
+            <li onClick={handleList} className={`text-5xl   relative  after:absolute after:left-[67px] after:top-1/2  after:w-[160px] after:h-16  ${active ? "after:bg-[white] " : "after:bg-transparent"} after:content-[""]  after:rounded-l-md ${active ? "text-[#5F35F5]  " : "text-white"} after:-translate-1/2   before:absolute before:top-1/2 before:-translate-y-1/2  before:-right-25  ${active ? "before:bg-[#5F35F5]" :"before:bg-transparent"} before:w-[10px] before:h-16 before:content-[""]  before:rounded-l-xl  before:z-19`}>  <IoSettingsOutline   className='relative top-0 left-0 z-10'  /> </li>
           
            
           </ul>
         </div>
         <div>
           <ul>
-               <li onClick={handleList} className='text-5xl   relative  after:absolute after:left-[67px] after:top-1/2  after:w-[160px] after:h-16  after:bg-[white] after:content-[""]  after:rounded-l-md text-[#5F35F5]  after:-translate-1/2   before:absolute before:top-1/2 before:-translate-y-1/2  before:-right-25 before:bg-[#5F35F5] before:w-[10px] before:h-16 before:content-[""]  before:rounded-l-xl  before:z-19 ' >  <LuLogOut   className='relative top-0 left-0 z-10' />  </li>
+               <li onClick={handleList} className={`text-5xl   relative  after:absolute after:left-[67px] after:top-1/2  after:w-[160px] after:h-16  ${active ? "after:bg-[white] " :"after:bg-transparent"} after:content-[""]  after:rounded-l-md ${active ? "text-[#5F35F5]" :"text-white"}  after:-translate-1/2   before:absolute before:top-1/2 before:-translate-y-1/2  before:-right-25 ${active ? "before:bg-[#5F35F5]": "before:bg-transparent"} before:w-[10px] before:h-16 before:content-[""]  before:rounded-l-xl  before:z-19 `}>  <LuLogOut   className='relative top-0 left-0 z-10' />  </li>
           </ul>
         
         </div>
