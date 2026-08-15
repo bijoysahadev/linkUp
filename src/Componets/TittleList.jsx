535bef12c8a56075a7bb6bb75b44788463ed2431
bijoysahadev <bijoysahamernstackdev@gmail.com>
2026-08-15 15:44:48 +0600

    feat firebase write operation setup succewssfully

--- src/Componets/TittleList.jsx
+++ src/Componets/TittleList.jsx
@@ -2,8 +2,24 @@ import React, { useState } from 'react'
 import Grid from '@mui/material/Grid';
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { ImCross } from "react-icons/im";
+import { getDatabase, ref, onValue, set, push } from "firebase/database";
+import toast, { Toaster } from 'react-hot-toast';
 const TittleList = ({tittle,className,addContent}) => {
+  const db = getDatabase()
   let [popup,setPopup]=useState(false)
+  let [groupname,setGroupname]=useState("")
+  let handleCreateGroup=()=>{
+    console.log(groupname);
+      set(push(ref(db, 'mygroup/')), {
+        groupname:groupname,
+        groupimage : "" ,
+
+    
+        }).then(()=> {
+          setPopup(false)
+          toast.success("Group Created")
+        })
+  }
   return (
   
      <div  > 
@@ -20,16 +36,17 @@ const TittleList = ({tittle,className,addContent}) => {
            <div className='flex flex-col items-center justify-between gap-5 ' >
             <ImCross  onClick={()=>setPopup(false)}  className='relative -top-[55px] left-[250px] text-black  text-2xl ' />
             <h2 className='text-white text-4xl font-bold ' >Group Name   :</h2>
-         <input  className=' w-[400px] border border-black rounded-md py-2 px-3' type="text" />
+         <input  value={groupname}  onChange={(e)=>setGroupname(e.target.value)}  className=' w-[400px] border border-black rounded-md py-2 px-3' type="text" />
      
          <div className='flex items-center justify-between gap-4 ' >
-            <button    className='text-white py-2 px-4 bg-red-500 rounded-2xl ' >Create</button>
+            <button  onClick={handleCreateGroup}   className='text-white py-2 px-4 bg-red-500 rounded-2xl ' >Create</button>
            <button   onClick={()=>setPopup(false)}  className='text-white py-2 px-4 bg-red-500 rounded-2xl' >Cancel</button>
          </div>
            </div>
         </div>
        </div>
         }
+           <Toaster />
        </div>
      </div>
   )
