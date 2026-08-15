63d797ad2c3c0266feb04ce2df88ffa0e69aa5f8
bijoysahadev <bijoysahamernstackdev@gmail.com>
2026-08-15 15:21:54 +0600

    feat:add fixes

--- src/Componets/TittleList.jsx
+++ src/Componets/TittleList.jsx
@@ -1,29 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Grid from '@mui/material/Grid';
 import { BsThreeDotsVertical } from "react-icons/bs";
+import { ImCross } from "react-icons/im";
 const TittleList = ({tittle,className,addContent}) => {
+  let [popup,setPopup]=useState(false)
   return (
   
      <div  > 
        <div className={` rounded-[20px] flex items-center justify-between   ${className} `} >
             <h3 className='text-base text-black font-semibold ' >{tittle}</h3>
    {
-     addContent && <button className='text-black bg-white py-3 px-3 rounded-md text-xs' >{addContent}</button>
+     addContent && <button  onClick={()=>setPopup(true) } className='text-black bg-white py-3 px-3 rounded-md text-xs' >{addContent}</button>
    }
        <BsThreeDotsVertical   className='text-[#5F35F5] text-sm   ' />
-       <div className='w-full h-full bg-[#0000000d] absolute top-0 left-0 z-50  flex justify-center items-center ' >
+        {
+          popup &&
+           <div className=' absolute w-full h-full bg-[#0000000d] absolute top-0 left-0 z-50  flex justify-center items-center ' >
         <div className='w-[600px] h-[500px] py-20 bg-amber-100 rounded-md  ' >
            <div className='flex flex-col items-center justify-between gap-5 ' >
+            <ImCross  onClick={()=>setPopup(false)}  className='relative -top-[55px] left-[250px] text-black  text-2xl ' />
             <h2 className='text-white text-4xl font-bold ' >Group Name   :</h2>
          <input  className=' w-[400px] border border-black rounded-md py-2 px-3' type="text" />
      
          <div className='flex items-center justify-between gap-4 ' >
-            <button  className='text-white py-2 px-4 bg-red-500 rounded-2xl ' >Create</button>
-           <button  className='text-white py-2 px-4 bg-red-500 rounded-2xl' >Cancel</button>
+            <button    className='text-white py-2 px-4 bg-red-500 rounded-2xl ' >Create</button>
+           <button   onClick={()=>setPopup(false)}  className='text-white py-2 px-4 bg-red-500 rounded-2xl' >Cancel</button>
          </div>
            </div>
         </div>
        </div>
+        }
        </div>
      </div>
   )
